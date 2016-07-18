fa747e17953ae6731bb5032425494e89ce5efd49
xraymonx <raymon_jesse2@hotmail.com>
2016-07-18 18:04:02 +0200

    Canvas input!

---
+++ MiniMechGameDev/Assets/Scripts/Input/Scroller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class Scroller : MonoBehaviour {
+
+	private Scrollbar _scrollBar;
+	private  Vector3 _oldPos; 
+	[SerializeField] private GameObject _playerMovement;
+	void Start()
+	{
+		_oldPos = transform.position;
+		_scrollBar = GetComponent<Scrollbar> ();
+
+	}
+	public void Pressed()
+	{
+		ChangeZ<IMoveable> (_playerMovement, x => x.ChangeMyZ (_scrollBar.value));
+	}
+
+	void ChangeZ<T>(GameObject target, Action<T> move)
+	{
+		T[] components = target.GetComponents<T> ();
+		foreach (T component in components) 
+		{
+			move (component);
+		}
+	}
+}
---
+++ MiniMechGameDev/Assets/Scripts/MovingPlayer/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IMoveable
+{
+	void ChangeMyZ (float value);
+}
+
+public class PlayerMovement : MonoBehaviour, IMoveable {
+
+	private Vector3 _newPos;
+ 	
+
+	void Start()
+	{
+		_newPos = transform.position;
+	}
+	public void ChangeMyZ(float value)
+	{
+		_newPos.z = 20 + (value * 10);
+		transform.position = _newPos;
+	}
+}

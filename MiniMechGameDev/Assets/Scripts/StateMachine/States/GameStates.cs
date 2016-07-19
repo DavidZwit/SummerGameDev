9f87d8f8d327d92af52a84eab1a76ecf61259d87
DavidZwit <davidzwitser@gmail.com>
2016-07-19 21:31:24 +0200

    fixing spawner

---
+++ MiniMechGameDev/Assets/Scripts/StateMachine/States/GameStates.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+
+namespace GameStates
+{
+    public class Idle : State
+    {
+        GameObject obj;
+        StatesEnum returnState;
+
+        public void Enter(GameObject theObject)
+        {
+
+        }
+
+        public bool Reason()
+        {
+
+            /* if (i should still be in this state) {
+               return true 
+
+            } else {
+                returnState = StatesEnum.(The state you want to go to)
+                return false;
+            }
+            
+             */
+            return true;
+        }
+
+        public void Act()
+        {
+
+        }
+
+        public StatesEnum Leave()
+        {
+            return returnState;
+        }
+    }
+
+
+    public class Slomo : State
+    {
+        GameObject obj;
+        StatesEnum returnState;
+
+        public void Enter(GameObject theObject)
+        {
+
+        }
+
+        public bool Reason()
+        {
+            return true;
+        }
+
+        public void Act()
+        {
+
+        }
+
+        public StatesEnum Leave()
+        {
+            return returnState;
+        }
+    }
+
+
+    public class Bad : State
+    {
+        GameObject obj;
+        StatesEnum returnState;
+
+        public void Enter(GameObject theObject)
+        {
+
+        }
+
+        public bool Reason()
+        {
+            return true;
+        }
+
+        public void Act()
+        {
+
+        }
+
+        public StatesEnum Leave()
+        {
+            return returnState;
+        }
+    }
+
+
+    public class Good : State
+    {
+        GameObject obj;
+        StatesEnum returnState;
+
+        public void Enter(GameObject theObject)
+        {
+
+        }
+
+        public bool Reason()
+        {
+            return true;
+        }
+
+        public void Act()
+        {
+
+        }
+
+        public StatesEnum Leave()
+        {
+            return returnState;
+        }
+    }
+}
---
+++ MiniMechGameDev/Assets/Scripts/StateMachine/States/StateTemplate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+class StateTemplate : State {
+
+    GameObject obj;
+    Animation anim;
+    string animString;
+    StatesEnum returnState;
+
+    public StateTemplate ()
+    {
+
+    }
+
+    public void Enter(GameObject theObject)
+    {
+        obj = theObject;
+    }
+
+    public bool Reason()
+    {
+        Debug.Log("reason");
+        return true;
+    }
+
+    public void Act()
+    {
+        Debug.Log("act");
+    }
+
+    public StatesEnum Leave()
+    {
+        Debug.Log("leave");
+        return returnState;
+    }
+}

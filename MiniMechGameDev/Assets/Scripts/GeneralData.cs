9d24c3b89cb7d48f79b57498bee3ff2fbd66c9b0
DavidZwit <davidzwitser@gmail.com>
2016-07-19 17:16:22 +0200

    InterChunckAdded

---
+++ MiniMechGameDev/Assets/Scripts/GeneralData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System;
+
+public struct NonDestroyableData
+{
+    public static List<Account> players = new List<Account>();
+    public static Account currPlayer;
+    
+    void SortPlayers()
+    {
+           
+    }
+}
+
+public struct Account
+{
+    int score;
+    string name;
+
+    public int Score
+    {
+        get { return score; }
+        set {  score = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set {
+            if (value.Length < 10)
+                name = value;
+            else name = "My name is really long";
+        }
+    }
+
+    public Account(int _score, string _name)
+    {
+        score = _score;
+        name = _name;
+    }
+    
+}

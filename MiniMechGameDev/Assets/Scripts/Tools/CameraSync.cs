654a51202079b5d1647d817a6be64861fc90c273
Ivo Geelhoed <ivogeelhoed@gmail.com>
2016-07-19 14:44:52 +0200

    Camera sync script + camera effects in preview scene

---
+++ MiniMechGameDev/Assets/Scripts/Tools/CameraSync.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraSync : MonoBehaviour
+{
+    public bool on = true;
+    public bool inGame = true;
+    public Camera designCam;
+    private Camera currentView;
+
+    public void OnDrawGizmos()
+    {
+        Synchornize();
+    }
+
+    void Synchornize()
+    {
+        if (on && SceneView.lastActiveSceneView.camera != null)
+        {
+            Camera lastView = SceneView.lastActiveSceneView.camera;
+            currentView = SceneView.lastActiveSceneView.camera;
+            designCam.transform.position = currentView.transform.position;
+            designCam.transform.rotation = currentView.transform.rotation;
+        }
+    }
+
+
+}
+#endif

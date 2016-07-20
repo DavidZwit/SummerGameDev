bda94a1fbabd1c039521f3c8bb2669c23b39b056
Ivo Geelhoed <ivogeelhoed@gmail.com>
2016-07-20 14:09:15 +0200

    Highscore system added ( no visual feedback yet )

--- MiniMechGameDev/Assets/Scripts/Ball.cs
+++ MiniMechGameDev/Assets/Scripts/Ball.cs
@@ -21,6 +21,49 @@ public class Ball : Singleton<Ball>
         set { origin = value; }
     }
 
+    private bool shotBall;
+    public bool ShotBall
+    {
+        get
+        {
+            return shotBall;
+        }
+        set
+        {
+            if (value) UpdatedCheck = false;
+            shotBall = value;
+        }
+    }
+
+    private bool UpdatedCheck = false;
+    private bool UpdateScore
+    {
+        get
+        {
+            if(ReceivedBall && Target != Origin && !UpdatedCheck)
+            {
+                UpdatedCheck = true;
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    private bool ReceivedBall
+    {
+        get
+        {
+            if (DistanceToTarget <= ReceiveOffset)
+            {
+                ShotBall = false;
+                return true;
+            }
+
+            return false;
+        }
+    }
+
     private float DistanceToTarget
     {
         get { return Vector3.Distance(this.transform.position, target.position); }
@@ -45,22 +88,27 @@ public class Ball : Singleton<Ball>
 	{
         if(Input.GetKeyDown(KeyCode.A))
         {
-            PassBall(GameObject.Find("P1_TEST").transform, 1, GameObject.Find("P2_TEST").transform);
+            PassBall(GameObject.Find("P1_TEST").transform, 5, GameObject.Find("P2_TEST").transform);
         }
         else if(Input.GetKeyDown(KeyCode.D))
         {
-            PassBall(GameObject.Find("P2_TEST").transform, 1, GameObject.Find("P1_TEST").transform);
+            PassBall(GameObject.Find("P2_TEST").transform, 5, GameObject.Find("P1_TEST").transform);
         }
 
-
-        if (DistanceToTarget >= ReceiveOffset)
+        if (!ReceivedBall)
             transform.position = Vector3.Lerp(transform.position, Target.position, PassSpeed * Time.deltaTime);
-	}
+
+
+        if (!UpdateScore) return;
+
+        ScoreManager.Instance.Scores.AddToScore(ScoreManager.Instance.SmallScore);
+        // Debug.Log("Current score : " + ScoreManager.Instance.Scores.CurrScore);
+    }
 
     public void PassBall(Transform t, float s, Transform o)
     {
+        ShotBall = true;
         // Debug.Log("Pass ball from : " + t.transform.position + ", to : " + o.transform.position);
-        Debug.Log("Target : " + t.name + ", Speed: " + s + ", Origin: " + o.name);
         Target = t;
         PassSpeed = s;
         Origin = o;
@@ -70,7 +118,7 @@ public class Ball : Singleton<Ball>
     {
         if(_col.tag == "Obstacles")
         {
-            Debug.Log("Hit an obstacle, attempting to go back to : " + Origin.transform.name);
+            // Debug.Log("Hit an obstacle, attempting to go back to : " + Origin.transform.name);
             PassBall(Origin, PassSpeed/1.5f, Origin);
             CameraShaker.Instance.shakeDuration = .4f;
         }
---
+++ MiniMechGameDev/Assets/Scripts/Score/Score.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Score
+{
+
+    public Score()
+    {
+        int currScore = 0;
+    }
+
+    private int currScore;
+    public int CurrScore
+    {
+        get { return currScore; }
+        set { currScore = value; }
+    }
+
+    public void AddToScore(int i)
+    {
+        CurrScore += i;
+    }
+
+    private int highScore;
+    public int HighScore
+    {
+        get { return highScore; }
+        set { highScore = value; }
+    }
+
+}

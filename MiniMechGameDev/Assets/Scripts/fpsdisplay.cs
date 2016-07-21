51c013b1ca217598b52910771cf264fcaecea46b
Ivo Geelhoed <ivogeelhoed@gmail.com>
2016-07-21 12:57:12 +0200

    Fixed FPS issues

---
+++ MiniMechGameDev/Assets/Scripts/fpsdisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class fpsdisplay : MonoBehaviour
+{
+
+    float FPS = 0.0f;
+    Text t;
+
+    void Awake()
+    {
+        t = this.transform.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        FPS = Mathf.Round( (1.0f / Time.deltaTime) );
+        t.text = FPS.ToString();
+    }
+
+}

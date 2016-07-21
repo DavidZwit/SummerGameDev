150410fa0bf55018278c1244d2514f6fa2a22eb6
tagor <Tagortrabajo@gmail.com>
2016-07-21 20:56:24 +0200

    feedbacks

---
+++ MiniMechGameDev/Assets/Scripts/ScoreAnim.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreAnim : MonoBehaviour {
+
+    // Use this for initialization
+    private int PreviouScore = 0;
+    private Animator AnimObjec;
+    private AudioSource Cheer;
+    void Start ()
+    {
+        AnimObjec = this.gameObject.GetComponent<Animator>();
+        Cheer = this.gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (ScoreManager.Instance.Scores.CurrScore > PreviouScore)
+        {
+            //Debug.Log("IVEDONEIT");
+            PreviouScore = ScoreManager.Instance.Scores.CurrScore;
+            AnimObjec.SetTrigger("Scored");
+            Cheer.Play();
+        }
+    }
+}
